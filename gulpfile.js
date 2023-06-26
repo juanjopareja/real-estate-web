4adf95701735866dde58a1d7b79f3bd78d028af4
juanjopareja <jupg25@msn.com>
2023-06-26 19:34:10 +0200

    Add initial files

---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+const { src, dest, watch, series } = require('gulp');
+
+// CSS and SASS
+const sass = require('gulp-sass')(require('sass'));
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const sourcemaps = require('gulp-sourcemaps');
+const cssnano = require('cssnano');
+
+// Images
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+const avif = require('gulp-avif');
+
+function css( done ) {
+    src('src/scss/app.scss')
+        .pipe( sourcemaps.init() )
+        .pipe( sass() )
+        .pipe( postcss([ autoprefixer(), cssnano() ]) )
+        .pipe( sourcemaps.write('.'))
+        .pipe( dest('build/css') )
+
+    done();
+}
+
+function images() {
+    return src('src/img/**/*')
+        .pipe( imagemin({ optimizationLevel: 3 }) )
+        .pipe( dest('build/img') )
+}
+
+function versionWebp() {
+    const options = {
+        quality: 50
+    }
+    return src('src/img/**/*.{png,jpg}')
+        .pipe( webp( options ) )
+        .pipe( dest('build/img') )
+}
+function versionAvif() {
+    const options = {
+        quality: 50
+    }
+    return src('src/img/**/*.{png,jpg}')
+        .pipe( avif( options ) )
+        .pipe( dest('build/img'))
+}
+
+function dev() {
+    watch( 'src/scss/**/*.scss', css );
+    watch( 'src/img/**/*', images );
+}
+
+
+exports.css = css;
+exports.dev = dev;
+exports.images = images;
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
+exports.default = series( images, versionWebp, versionAvif, css, dev  );
